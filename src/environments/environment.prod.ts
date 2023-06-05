be4496533dde8560c33a062173d38bb90d66bfa3
jvelez-code <jaimev_tec@jaimetorres.net>
2023-06-05 17:43:27 -0500

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
+  HOST: 'http://10.10.11.198:9898',
   production: true,
-  HOST: 'http://10.10.1.198:9898',
   TOKEN_AUTH_USERNAME: 'mitomediapp',
   TOKEN_AUTH_PASSWORD: 'mito89codex',
   TOKEN_NAME: 'access_token',
